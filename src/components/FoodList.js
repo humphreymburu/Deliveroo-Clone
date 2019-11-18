a2327712419e91cc3fe1b544885fccbe55875770
humphrey <humphreymuhiu@aakenya.co.ke>
2019-11-18 23:26:15 +0300

    starter files

---
+++ src/components/FoodList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Food from './Food'
+
+export default class FoodList extends Component {
+  render() {
+    return (
+      <div>
+          <Food />
+      </div>
+    )
+  }
+}
